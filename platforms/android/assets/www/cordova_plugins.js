002e73dc56b9432d90aed6677a2465ec0e9b7c14
liveu <liveu@naver.com>
2015-07-25 08:22:42 +0900

    150725

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.maleficus.hybrid.MaleficusJSBridgePlugin/www/MaleficusJSBridgePlugin.js",
+        "id": "com.maleficus.hybrid.MaleficusJSBridgePlugin.MaleficusJSBridgePlugin",
+        "clobbers": [
+            "MaleficusJSBridgePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.maleficus.hybrid.MaleficusJSBridgePlugin": "0.2.11"
 }
 // BOTTOM OF METADATA
 });
